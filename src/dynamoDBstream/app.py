ce2db398e1c1ab21fa15a32db02bbe9163224e79
JAVIER\jhmar <jhmarin3@gmail.com>
2024-08-25 12:49:34 -0700

    I added: DBStream Linked a Lambda with DBStream Create SQS for Event Bridge rule Create Event Bridge rule Create SQS DLQ for SQS Event bridge rule.

---
+++ src/dynamoDBstream/app.py
@@ -0,0 +1,56 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+eventbridge = boto3.client('events')
+
+def lambda_handler(event, context):
+    try:
+        if 'Records' not in event:
+            raise ValueError("Event does not contain 'Records' key")
+            
+        for record in event['Records']:
+            if record['eventName'] in ['INSERT', 'MODIFY', 'REMOVE']:
+                event_detail = {
+                    'eventName': record['eventName'],
+                    'dynamodb': record['dynamodb']
+                }
+                # Emit an event to EventBridge
+                eventbridge.put_events(
+                    Entries=[
+                        {
+                            'Source': 'loanpro.reservation.service',
+                            'DetailType': 'ReservationCreated',
+                            'Detail': json.dumps(event_detail),
+                            'EventBusName': 'default'  # Use a custom event bus if needed
+                        }
+                    ]
+                )
+                # eventbridge.put_events(
+                #     Entries=[
+                #         {
+                #             'Source': 'my.reservation',
+                #             'DetailType': 'ReservationCreated',
+                #             'Detail': json.dumps({
+                #                 'reservationId': '1',
+                #                 'user': 'test',
+                #                 'details': 'details'
+                #             }),
+                #             'EventBusName': 'default'  # Use a custom event bus if needed
+                #         }
+                #     ]
+                # )
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Processed records successfully!')
+        }
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': f'Error creating reservation: {str(e)}'})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': f'Unexpected error: {str(e)}'})
+        }
